cf630d3a615b81963d14063d745bce9247ef0084
Arthur Stolyar <nekr.fabula@gmail.com>
2018-05-11 22:36:54 +0300

    Delete dev artifacts

--- src/App.js
+++ src/App.js
@@ -12,11 +12,3 @@ export default class App extends Component {
     </Provider>;
   }
 }
-
-/*BITSTAMP_SPOT_BTC_USD
-GET /v1/ohlcv/BITSTAMP_SPOT_BTC_USD/latest?period_id=5YRS
-
-fetch('https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/latest?period_id=1DAY', {
-  mode: 'cors',
-  headers: { 'X-CoinAPI-Key': '17C2C3D9-B6BA-48CF-B091-85CC61A66A6D' }
-})*/
